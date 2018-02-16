a6c739ceaffe28775202ab3cab8ba9a1784588ac
Lester Emerson <lesphilly@gmail.com>
2018-02-16 22:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function makeInt(n) {
   return parseInt(n, 10);
 }
 function preserveDecimal(n){
-  return parseFloat('2.222', 10)
+  return parseFloat('2.222', 10);
 }
 function preserveDecimal(n) {
   return parseFloat('sldkjflksjf');
